6302b03229cd630c086d237853781ac7299a9a3c
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-23 02:19:26 -0700

    test request to api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { __API__ } from "./utilities/constants";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    axios.get(__API__ + "/accounts").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+  return <div>sup</div>;
 }
 
 export default App;
