8c57923140f8736fe0dce877d364d91f399672c4
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-25 20:29:05 -0700

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { __API__ } from "./utilities/constants";
 import LoginButton from "./components/buttons/login";
 
