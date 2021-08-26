e10732d40b3ce393b90d67726b60312a20787661
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-26 12:11:23 -0700

    change useEffect

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import LoginButton from "./components/buttons/login";
 
 function App() {
   const [accessToken, setAccessToken] = useState("");
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     setAccessToken(document.cookie.split("=")[1] || "");
-  });
+  },[]);
   return (
     <div>
-      <h1>accessToken: {accessToken || "none"}</h1>
       <LoginButton url={__API__ + "/login"}></LoginButton>
+      <h1>accessToken: {accessToken || "none"}</h1>
     </div>
   );
 }
