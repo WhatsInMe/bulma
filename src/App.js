de3ad159406cdbf431d605642d70360d49510e30
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-26 12:49:36 -0700

    get cookie with name

--- src/App.js
+++ src/App.js
@@ -5,14 +5,30 @@ import LoginButton from "./components/buttons/login";
 
 function App() {
   const [accessToken, setAccessToken] = useState("");
+  const [secretMessage, setSecretMessage] = useState("");
   const [posts, setPosts] = useState([]);
+
+  const getCookieName = (name, cookie) => {
+    if (!cookie) {
+      return "";
+    }
+    const pair = cookie.split("; ").find((e) => e.split("=")[0] === name);
+    if (pair) {
+      return pair.split("=")[1];
+    }
+    return "";
+  };
+
   useEffect(() => {
-    setAccessToken(document.cookie.split("=")[1] || "");
-  },[]);
+    setAccessToken(getCookieName("accessToken", document.cookie));
+    setSecretMessage(getCookieName("secretMessage", document.cookie));
+  }, []);
+
   return (
     <div>
       <LoginButton url={__API__ + "/login"}></LoginButton>
-      <h1>accessToken: {accessToken || "none"}</h1>
+      <h1>accessToken: {accessToken || "empty"}</h1>
+      <h1>secretMessage: {secretMessage || "empty"}</h1>
     </div>
   );
 }
