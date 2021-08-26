aa8c2b664dd642118855390d263f24e86c5fabd7
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-25 20:21:11 -0700

    save access token to state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { __API__ } from "./utilities/constants";
 import LoginButton from "./components/buttons/login";
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+  useEffect(() => {
+    setAccessToken(document.cookie.split("=")[1] || "");
+  });
   return (
     <div>
-      <h1>Login:</h1>
+      <h1>accessToken: {accessToken || "none"}</h1>
       <LoginButton url={__API__ + "/login"}></LoginButton>
-      {/* <LoginButton url="/login"></LoginButton> */}
     </div>
   );
 }
