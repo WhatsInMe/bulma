162c30d37044757baa63a028f1b3736b73c68951
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-26 20:26:03 -0700

    conditional render

--- src/components/asdf.js
+++ src/components/asdf.js
@@ -1,28 +1,43 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import { __API__ } from "../utilities/constants";
 import { getCookie } from "../utilities/functions";
 
 const Asdf = (props) => {
+  const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     axios
       .get(__API__ + "/posts", {
         headers: {
-          Authorization: `Bearer ${getCookie("accessToken")}`,
-          //   Authorization: `Bearer test1`,
+          //   Authorization: `Bearer ${getCookie("accessToken")}`,
+          Authorization: `Bearer test1`,
         },
       })
       .then((res) => {
         console.log("yes");
         console.log(res.data);
+        setPosts(res.data);
       })
       .catch(() => {
         console.log("no");
       });
   }, []);
 
-  return <h1>test component</h1>;
+  return (
+    <div>
+      {posts.length ? (
+        <div>
+          <h1>something</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>nothing</h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Asdf;
